39c37af7bda33896c0b006f99bfaa0e90c7d8615
manisha sanjay chavan <manisharathod8975@gmail.com>
2022-08-18 02:51:38 -0700

    project added central repository

---
+++ src/main/java/com/BikkadIT/PhoneBookAppJpaRepositoryApplication.java
@@ -0,0 +1,13 @@
+package com.BikkadIT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhoneBookAppJpaRepositoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhoneBookAppJpaRepositoryApplication.class, args);
+	}
+
+}
